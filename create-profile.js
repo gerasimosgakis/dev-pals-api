fac6142d25241c92349aee3b6e8e23a6996855e8
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-02-17 17:36:33 +0000

    feat: refactor code to be reusable

--- create-profile.js
+++ create-profile.js
@@ -1,9 +1,8 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -31,42 +30,49 @@ export function main(event, context, callback) {
       profileId: uuid.v1(),
       handle: data.handle,
       company: data.company,
-      // website: data.website,
-      // location: data.location,
-      // status: data.status,
-      // skills: data.skills,
-      // githubusername: data.githubusername,
-      // experience: data.experience,
-      // education: data.education,
-      // social: data.social,
+      website: data.website,
+      location: data.location,
+      status: data.status,
+      skills: data.skills,
+      githubusername: data.githubusername,
+      experience: data.experience,
+      education: data.education,
+      social: data.social,
       createdAt: Date.now()
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
 
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
+  // dynamoDb.put(params, (error, data) => {
+  //   // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+  //   const headers = {
+  //     "Access-Control-Allow-Origin": "*",
+  //     "Access-Control-Allow-Credentials": true
+  //   };
 
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  //   // Return status code 500 on error
+  //   if (error) {
+  //     const response = {
+  //       statusCode: 500,
+  //       headers: headers,
+  //       body: JSON.stringify({ status: false })
+  //     };
+  //     callback(null, response);
+  //     return;
+  //   }
+
+  //   // Return status code 200 and the newly created item
+  //   const response = {
+  //     statusCode: 200,
+  //     headers: headers,
+  //     body: JSON.stringify(params.Item)
+  //   };
+  //   callback(null, response);
+  // });
 }
