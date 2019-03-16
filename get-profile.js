1dc0f6e26a0a65798076fc545637e1a39828831f
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-03-16 14:27:55 +0000

    wip

--- get-profile.js
+++ get-profile.js
@@ -7,17 +7,21 @@ export async function main(event, context) {
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
-    Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      profileId: event.pathParameters.id
+    // Key: {
+    //   // userId: event.requestContext.identity.cognitoIdentityId
+    //   //profileId: event.pathParameters.id
+    // }
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.pathParameters.id
     }
   };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
+    const result = await dynamoDbLib.call("query", params);
+    if (result.Items) {
       // Return retrieved item
-      return success(result.Item);
+      return success(result.Items);
     } else {
       return failure({ status: false, error: "Item not found" });
     }
